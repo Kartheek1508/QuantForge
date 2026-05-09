a1b2e1573e10311f8dc51be4b906d0503f9936c7
yeshwantha29-svg <yeshwanth.a29@gmail.com>
2026-05-09 14:14:02 +0530

    Adding data.py to QuantForge

---
+++ QuantForge/data.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+def monte_carlo_data(ticker:str,start_date:str,end_date:str):
+    ticker=ticker.strip().upper()
+    if ticker  == "":
+        raise ValueError("Ticker cannot be empty")
+    try:
+        data=yf.download(ticker,start=start_date,end=end_date)
+    except Exception:
+        raise ValueError(f"Download failed for {ticker}")
+    if data.empty:
+        raise ValueError(f"No data found for {ticker}")  
+    close=data["Close"]
+    close=close.dropna()
+    if close.empty:
+        raise ValueError(f"No valid closing prices fo {ticker}")
+    return close
+
