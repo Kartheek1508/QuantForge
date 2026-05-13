578d1a837da91c500ebb24123cae577232681697
yeshwantha29-svg <yeshwanth.a29@gmail.com>
2026-05-13 17:27:44 +0530

    Updated data.py with new changes

--- data.py
+++ data.py
@@ -15,3 +15,21 @@ def monte_carlo_data(ticker:str,start_date:str,end_date:str):
         raise ValueError(f"No valid closing prices fo {ticker}")
     return close
 
+
+def reinforcement_data(ticker:str,start_date:str,end_date:str):
+    ticker=ticker.strip().upper()
+    if ticker  == "":
+        raise ValueError("Ticker cannot be empty")
+    try:
+        data=yf.download(ticker,start=start_date,end=end_date)
+    except Exception:
+        raise ValueError(f"Download failed for {ticker}")
+    if data.empty:
+        raise ValueError(f"No data found for {ticker}")  
+    close=data["Close"]
+    close=close.ffill()
+   
+    close=close.dropna()
+    if close.empty:
+        raise ValueError(f"No valid closing prices fo {ticker}")
+    return close
